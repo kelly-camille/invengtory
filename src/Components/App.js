e89369ad676bd86845ad0057b4effde0a7565f0a
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-11-18 11:38:19 -0800

    create component folder, add app, control, and inventory component files

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Control from "./Control";
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <Control />
+    </React.Fragment>
+  );
+}
+
+export default App;
